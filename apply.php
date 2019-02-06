eeb4f6ffbb2db6f3c86a4e82899ec930b6476d9e
iqrakafayat <40511965+iqrakafayat@users.noreply.github.com>
2019-02-06 11:54:03 +0500

    Add files via upload

---
+++ apply.php
@@ -0,0 +1,5 @@
+<?php
+$connection = mysqli_connect('localhost','root','','army_db');
+if(!$connection)
+    die("'army_db' database not connected.");
+/* Question 2:  Write the code below accordingly */
